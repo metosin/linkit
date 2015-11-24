b0d84d46bdd00020f47066a7c05ed763bbb01691
Juho Teperi <juho.teperi@iki.fi>
2015-11-24 22:16:17 +0200

    Clean backend

--- src/clj/backend/api.clj
+++ src/clj/backend/api.clj
@@ -6,46 +6,59 @@
             [metosin.dates :as dates]
             [monger.operators :refer :all]
             [monger.collection :as mc]
-            [monger.joda-time]
-            [clojure.java.io :as io]
-            [clj-http.client :as http]
-            [ring.util.http-predicates :refer [ok?]]
-            [potpuri.core :as p]
-            [net.cgrand.enlive-html :as html]))
+            monger.joda-time
+            [backend.impl :as impl]))
 
-(defnk ^:query all [db]
+;;
+;; Context handlers
+;;
+
+(defn load-link
+  "Ensures that there is a link-id parameter. Associates the referred link
+  to context or returns an error if it doesn't exist."
+  [_]
+  (fnk [db [:data link-id :- s/Str] :as ctx]
+    (if-let [link  (mc/find-map-by-id db :links link-id)]
+      (assoc-in ctx [:entity :link] link)
+      (failure! {:status :link-doesnt-exist
+                 :link-id link-id}))))
+
+(defn require-liked
+  "Ensures that the user has or has not liked the referred link."
+  [liked?]
+  (fnk [[:entity [:link likeUsers]]
+        [:data user]
+        :as context]
+    (if (= liked? (contains? (set likeUsers) user))
+      context
+      (failure! {:status (if liked? :cant-dislike-not-liked :cant-like-already-liked)
+                 :liked? liked?}))))
+
+;;
+;; Queries
+;;
+
+(defnk ^:query all
+  "Returns all the links"
+  [db]
   (success (mc/find-maps db :links nil {:likeUsers 0})))
 
-(defnk ^:query by-id [db [:data link-id]]
-  (success (mc/find-map-by-id db :links link-id {:likeUsers 0})))
+(defnk ^:query by-id
+  "Returns a single link by given id"
+  {::load-link true}
+  [db [:entity link]]
+  (success link))
 
-(defn get-info [url]
-  (let [res (http/get url {:as :stream})]
-    (if (ok? res)
-      (let [url' (java.net.URL. url)
-            baseurl (str (.getProtocol url') "://" (.getHost url'))
-            html (html/html-resource (:body res))
-            icons (into {} (map (juxt :sizes :href) (map :attrs (html/select html [:head [:link (html/attr-has :rel "icon")]]))))
-            icon (or (get icons "96x96")
-                     (get icons "64x64")
-                     (get icons "32x32")
-                     (get icons "16x16")
-                     (get icons nil))
-            icon-url (if icon
-                       (cond
-                         (.startsWith icon "http") icon
-                         (.startsWith icon "/") (str baseurl icon)
-                         :else (str baseurl "/" icon)))]
-        {:title (html/text (first (html/select html [:head :title])))
-         :favicon icon-url}))))
+;;
+;; Commands
+;;
 
-(comment
-  (get-info "http://clojars.org"))
-
-(defnk ^:command add [db, data :- domain/NewLink]
+(defnk ^:command add
+  "Adds a single link to database"
+  [db, data :- domain/NewLink]
   (try
     (mc/insert db :links (merge data
-                                (get-info (:url data))
+                                (impl/get-info (:url data))
                                 {:dateTime (dates/now)
                                  :_id (str (org.bson.types.ObjectId.))
                                  :likes 0
@@ -54,38 +67,11 @@
     (catch Exception e
       (failure {:status :error}))))
 
-(defn load-link [_]
-  (fnk [db [:data link-id :- s/Str] :as ctx]
-    (if-let [link  (mc/find-map-by-id db :links link-id)]
-      (assoc-in ctx [:entity :link] link)
-      (failure! {:status :link-doesnt-exist
-                 :link-id link-id}))))
-
-(defn require-liked [liked?]
-  (fnk [[:entity [:link likeUsers]]
-        [:data user]
-        :as context]
-    (println likeUsers user liked?)
-    (if (= liked? (contains? (set likeUsers) user))
-      context)))
-
-(defn require-liked-or-fail [liked?]
-  (some-fn
-    (require-liked liked?)
-    (fn [context]
-      (failure! {:status (if liked? :cant-dislike-not-liked :cant-like-already-liked)
-                 :liked? liked?}))))
-
 (defnk ^:command like
   "Adds like to the link if user hasn't liked this link already."
   {::load-link true
    ::liked? false}
   [db, [:data link-id :- s/Str user :- s/Str]]
-  ; This is only executed if pre-requisitest are filled:
-  ; - Link exists
-  ; - User doesn't like this link already
-  ;
-  ; Due to Mongo, the update is idempotent
   (mc/update db :links
              {:_id link-id
               :likeUsers {$ne user}}
@@ -104,9 +90,3 @@
              {$pull {:likeUsers user}
               $inc {:likes -1}})
   (success {:status :ok}))
-
-(comment
-  (get-links (user/ctx))
-  (add-link (merge (user/ctx) {:data {:url "http://clojars.org"}}))
-  (like-link (merge (user/ctx) {:data {:link-id "5650b5e6f7ff9b5ff023077b" :user "Foo"}}))
-  (dislike-link (merge (user/ctx) {:data {:link-id "5650b5e6f7ff9b5ff023077b" :user "Foo"}})))
---
+++ src/clj/backend/impl.clj
@@ -0,0 +1,32 @@
+(ns backend.impl
+  (:require [clojure.java.io :as io]
+            [clj-http.client :as http]
+            [ring.util.http-predicates :refer [ok?]]
+            [potpuri.core :as p]
+            [net.cgrand.enlive-html :as html]))
+
+(defn get-info
+  "Scrapes the given url's title and favicon."
+  [url]
+  (let [res (http/get url {:as :stream})]
+    (if (ok? res)
+      (let [url' (java.net.URL. url)
+            baseurl (str (.getProtocol url') "://" (.getHost url'))
+            html (html/html-resource (:body res))
+            icons (into {} (map (juxt :sizes :href) (map :attrs (html/select html [:head [:link (html/attr-has :rel "icon")]]))))
+            icon (or (get icons "96x96")
+                     (get icons "64x64")
+                     (get icons "32x32")
+                     (get icons "16x16")
+                     (get icons nil))
+            icon-url (if icon
+                       (cond
+                         (.startsWith icon "http") icon
+                         (.startsWith icon "/") (str baseurl icon)
+                         :else (str baseurl "/" icon)))]
+        {:title (html/text (first (html/select html [:head :title])))
+         :favicon icon-url}))))
+
+(comment
+  (get-info "http://clojars.org"))
+
